6961b41b4c1a7daf066592131ddffed6378699a2
Bishop <daunknown11@gmail.com>
2026-01-03 12:09:02 -0600

    docker + tf serving complected

--- api/main.py
+++ api/main.py
@@ -1,27 +1,22 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
-import tensorflow as tf 
-import keras
+import tensorflow as tf
 import requests
-
 
 app = FastAPI()
 
 
-endpoint = "http://localhost:8501/v1/models/potaotes_model:predict"
+endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
 
-
-
-CLASS_NAMES  = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
-
-
+CLASS_NAMES = ["Potato__Early_blight", "Potato__Late_blight", "Potato__health"]
 
 @app.get("/ping")
 async def ping():
-    return "Hello, I am alive" 
+    return "Hello, I am alive"
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
@@ -33,13 +28,21 @@ async def predict(
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-    
-    json_data{"instances": img_batch.tolist()}
 
-    response = requests.post(endpoint, json = json_data)
+    json_data = {
+        "instances": img_batch.tolist()
+    }
 
+    response = requests.post(endpoint, json=json_data)
+    prediction = np.array(response.json()["predictions"][0])
 
-    pass
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence = np.max(prediction)
+
+    return {
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port = 8000)
+    uvicorn.run(app, host='localhost', port=8000)
